ba87df40c39a119245b29e89cc7b8998ca40813d
NakataL13 <104929633+NakataL13@users.noreply.github.com>
2022-05-19 16:19:20 +0300

    First commit

---
+++ index.vue
@@ -0,0 +1,19 @@
+<script>
+import NavBar from '../components/NavBar.vue';
+import Footer from '../components/Footer.vue';
+import Content from '../components/Content.vue';
+
+
+  export default{
+    layout: "custom",
+    components: { NavBar, Footer, Content }
+}
+</script>
+<template>
+ <div>
+   <NavBar/>
+   <moreHero/>
+   <Content/>
+   <Footer/>
+ </div>
+</template>
---
+++ posts/_id.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <PostsTable
+        v-for="post in posts"
+        :key="id"
+        />
+    </div>
+</template>
+
+<script>
+import Posts from './index.vue'
+    export default{
+    data() {
+        return {
+            props: ["id", "title", "body", "posts"]
+        };
+    },
+    components: { Posts }
+}
+
+</script>
